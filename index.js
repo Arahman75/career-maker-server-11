8c05f72fd1154f7510a8df745e026c62dc2395e7
Arahman75 <selfarahman01825@gmail.com>
2023-11-06 13:59:55 +0600

    added the get single service api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000
 
@@ -9,15 +9,8 @@ const port = process.env.PORT || 5000
 app.use(express.json());
 app.use(cors());
 
-// pass : kqIAhn6Rsv8nb7RY
-// user: petsWalking
-console.log(process.env.DB_PASS);
-console.log(process.env.DB_USER);
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ik9fyhp.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri, 'hello secret');
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -31,22 +24,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
         const serviceCollection = client.db('petsDB').collection('services');
 
         //post service
 
-        app.post('/services', async (req, res) => {
-            const newProduct = req.body;
-            console.log(newProduct);
-            const result = await productCollection.insertOne(newProduct);
-            res.send(result)
-        })
-
-        // app.get('/products', async (req, res) => {
-        //     const cursor = productCollection.find();
-        //     const result = await cursor.toArray();
+        // app.post('/services', async (req, res) => {
+        //     const newProduct = req.body;
+        //     console.log(newProduct);
+        //     const result = await serviceCollection.insertOne(newProduct);
         //     res.send(result)
         // })
+
+        // get all services
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
 
         // app.delete('/products/:id', async (req, res) => {
@@ -56,13 +51,14 @@ async function run() {
         //     res.send(result)
         // })
 
-        // app.get('/products/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     console.log('update id', id);
-        //     const query = { _id: new ObjectId(id) };
-        //     const result = await productCollection.findOne(query);
-        //     res.send(result)
-        // })
+        // get single service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('update id', id);
+            const query = { _id: new ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result)
+        })
 
         // app.put('/products/:id', async (req, res) => {
         //     const id = req.params.id;
@@ -88,16 +84,16 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Pets server is running!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Pets service is running on port ${port}`)
 })
