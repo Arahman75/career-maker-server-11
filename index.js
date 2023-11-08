654f5aa37706ef9693e1fe5d4a9e479401dff7f1
Arahman75 <selfarahman01825@gmail.com>
2023-11-08 07:01:40 +0600

    added the delete api

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ async function run() {
             res.send(result)
         })
 
+        // handle delete
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // post booking
         app.post('/booking', async (req, res) => {
@@ -109,12 +117,7 @@ async function run() {
         //     res.send(result)
         // })
 
-        // app.delete('/bookings/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) };
-        //     const result = await bookingCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+
 
 
 
